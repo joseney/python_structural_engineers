93382cda85d02c0993b79f01a8e05271527eed5c
joseney <joseney_moro@yahoo.com.br>
2021-09-16 08:17:49 -0300

    atual
    Signed-off-by: joseney <joseney_moro@yahoo.com.br>

--- 02_creating_arrays.py
+++ 02_creating_arrays.py
@@ -1,9 +1,9 @@
 import numpy as np
 a = np.array([1,2,3,4])
-b = np.array([[1,2],[5,4]])
+b = np.array([[1,2],[3.5,4.2]])
 
-print("a=0".format(a))
-print("b=0".format(a))
+print("a={0}".format(a))
+print("b={0}".format(b))
 
-print("a=0".format(a))
+
 
